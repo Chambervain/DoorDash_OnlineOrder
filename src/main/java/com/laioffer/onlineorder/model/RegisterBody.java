774fc8aa4c79e183a601000c1f78e69962cdae71
Bowen Zhang <bz2306@nyu.edu>
2023-06-10 21:43:59 -0400

    Integrate react js files from front end with spring boot project of the back end

--- src/main/java/com/laioffer/onlineorder/model/RegisterBody.java
+++ src/main/java/com/laioffer/onlineorder/model/RegisterBody.java
@@ -5,8 +5,8 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 public record RegisterBody(
         String email,
         String password,
-        @JsonProperty("first_name") String firstName,
-        @JsonProperty("last_name") String lastName
+        String firstName,
+        String lastName
 
 ) {
 }
